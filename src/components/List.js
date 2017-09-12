970875a7ae0a6054582a1c79820ef54ae14d383f
DavidMichaelPena <alexamenos@gmail.com>
2017-09-12 00:04:35 +0000

    NoteCard component

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,29 @@
-import React from 'react';
-class List extends React.Component {
+import React, { Component } from 'react';
+import NoteCard from './NoteCard';
+
+
+class List extends Component {
 
   componentWillMount() {
     this.props.getNotes();
   }
 
   render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+
     return (
       <div className="list-container">
-      List Component
+      {cards}
       </div>
     );
   }
